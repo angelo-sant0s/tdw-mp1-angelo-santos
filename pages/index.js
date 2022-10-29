5500ac5ee965eb530667f30e1c0bed2e546b955d
angelo-sant0s <80397514+angelo-sant0s@users.noreply.github.com>
2022-10-29 17:55:57 +0100

    LINDO

--- pages/index.js
+++ pages/index.js
@@ -8,36 +8,36 @@ import Head from 'next/head'
 import { CMS_NAME } from '../lib/constants'
 
 export default function Index({ preview, allPosts }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
-  return (
-    <>
-      <Layout preview={preview}>
-        <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
-        </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
-      </Layout>
-    </>
-  )
+    const heroPost = allPosts[0]
+    const morePosts = allPosts.slice(1)
+    return (
+        <>
+            <Layout preview={preview}>
+                <Head>
+                    <title>Next.js Blog Example with {CMS_NAME}</title>
+                </Head>
+                <Container>
+                    <Intro />
+                    {heroPost && (
+                        <HeroPost
+                            title={heroPost.title}
+                            coverImage={heroPost.coverImage}
+                            date={heroPost.date}
+                            author={heroPost.author}
+                            slug={heroPost.slug}
+                            excerpt={heroPost.excerpt}
+                        />
+                    )}
+                    {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+                </Container>
+            </Layout>
+        </>
+    )
 }
 
 export async function getStaticProps({ preview = false }) {
-  const allPosts = (await getAllPostsForHome(preview)) ?? []
-  return {
-    props: { preview, allPosts },
-  }
+    const allPosts = (await getAllPostsForHome(preview)) ?? []
+    return {
+        props: { preview, allPosts },
+    }
 }
